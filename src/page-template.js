085a3dcd267bdf3210cccdba1a9d8e07d960288d
Imperial Design <amsanchez@knox.edu>
2020-12-18 07:05:19 -0600

    found/fixed error, manager card set-up, included 'role' in lib

--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,8 @@
-const createTeamProfile = Profile => {
+const createProfile = profile => {
 const createManager = manager => {
     console.log(manager.getName());
-    return () => {
-<div class="card">
+    return `
+<div class="card"> 
     <div class="header">
         <h1>${manager.getName()}</h1>
         <h2>${manager.getRole()}</h2>
@@ -13,12 +13,40 @@ const createManager = manager => {
         <li class="employeeInfo">Office Number: ${manager.getOfficeNumber()}</li>
     </div>
 </div>
-}}};
+`
+};
 
 const createHTML = [];
-createHTML.push(profile.filter(employee=> employee,getRole() === "Manager")
+createHTML.push(profile.filter(employee=> employee.getRole() === "Manager")
 .map(manager => createManager (manager))
 );
 
 return createHTML.join("");
+
+}
+
+module.exports = profile => {
+    console.log(profile);
+return `
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Profile Generator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="../dist/style.css" />
+    </head>
+    <body>
+        <header>
+            <h1>TEAM PROFILE</h1>
+        </header>
+        <div>
+            <div class="cards">
+                ${createProfile(profile)}
+            </div>
+        </div>  
+    </body>
+</html>
+`
+}
 
