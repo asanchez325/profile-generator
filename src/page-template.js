4200f92660796321f50072b2b8cc3449201ac437
Imperial Design <amsanchez@knox.edu>
2020-12-18 11:15:24 -0600

    start adding style

--- src/page-template.js
+++ src/page-template.js
@@ -2,33 +2,37 @@ const createProfile = profile => {
 const createManager = manager => {
     console.log(manager.getName());
     return `
-<div class="card"> 
-    <div class="header">
-        <h1>${manager.getName()}</h1>
-        <h2>${manager.getRole()}</h2>
+<div class="card">
+<div class="card-manager">
+        <div class="card-role">
+            <h2>${manager.getRole()}</h2> 
+        </div>
+        <div class="card-content">
+            <h1>${manager.getName()}</h1>
+            <li class="employeeInfo">ID: ${manager.getId()}</li>
+            <li class="employeeInfo"><a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="employeeInfo">Office Number:${manager.getOffice()}</li>
+         </div>
     </div>
-    <div class="body">
-        <li class="employeeInfo">ID: ${manager.getId()}</li>
-        <li class="employeeInfo"><a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-        <li class="employeeInfo">Office Number:${manager.getOffice()}</li>
     </div>
-</div>
 `
 };
 
 const createEngineer = engineer => {
     console.log(engineer.getName());
     return `
-<div class="card"> 
-    <div class="header">
+<div class="card">
+<div class="card-engineer">
+        <div class="card-role">
+            <h2>${engineer.getRole()}</h2>
+        </div>
+    <div class="card-content">
         <h1>${engineer.getName()}</h1>
-        <h2>${engineer.getRole()}</h2>
-    </div>
-    <div class="body">
         <li class="employeeInfo">ID: ${engineer.getId()}</li>
         <li class="employeeInfo"><a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
         <li class="employeeInfo">Office Number: ${engineer.getGithub()}</li>
     </div>
+</div>
 </div>
 `
 };
@@ -36,16 +40,19 @@ const createEngineer = engineer => {
 const createIntern = intern => {
     console.log(intern.getName());
     return `
-<div class="card"> 
-    <div class="header">
-        <h1>${intern.getName()}</h1>
-        <h2>${intern.getRole()}</h2>
+<div class="card">
+<div class="card-intern">
+    <div class="card-role">
+            <h2>${intern.getRole()}</h2>
+        </div>
+        <div class="card-content">
+            <h1>${intern.getName()}</h1>
+            <li class="employeeInfo">ID: ${intern.getId()}</li>
+            <li class="employeeInfo"><a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="employeeInfo">Office Number: ${intern.getSchool()}</li>
+        </div>
     </div>
-    <div class="body">
-        <li class="employeeInfo">ID: ${intern.getId()}</li>
-        <li class="employeeInfo"><a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-        <li class="employeeInfo">Office Number: ${intern.getSchool()}</li>
-    </div>
+</div>
 </div>
 `
 };
@@ -84,7 +91,7 @@ return `
             <h1>TEAM PROFILE</h1>
         </header>
         <div>
-            <div class="cards">
+            <div class="card-section">
                 ${createProfile(profile)}
             </div>
         </div>  
